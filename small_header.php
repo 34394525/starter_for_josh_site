41ff1c00f66a753e43208a3ef1c96e4cb75aebf3
34394525 <34394525@student.murdoch.edu.au>
2024-04-12 08:48:35 +0800

    Update small_header.php

--- small_header.php
+++ small_header.php
@@ -52,7 +52,19 @@
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar" id="buttonTitle" title="Title">
           <span class="navbar-toggler-icon"></span>
         </button>
+
 <?php
+wp_nav_menu( array(
+  'theme_location'  => 'primary',
+  'depth'           => 2, // 1 = no dropdowns, 2 = with dropdowns.
+  'container'       => 'div',
+  'container_class' => 'collapse navbar-collapse',
+  'container_id'    => 'bs-example-navbar-collapse-1',
+  'menu_class'      => 'navbar-nav ml-auto',
+  'fallback_cb'     => 'WP_Bootstrap_Navwalker::fallback',
+  'walker'          => new WP_Bootstrap_Navwalker(),
+) );
+
 ?>
       </nav>
 <div class="smallpage-tag">
